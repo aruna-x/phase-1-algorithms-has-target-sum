f5b093777ea7d8d125fe9adaf35fcd12bb0bbe52
Aruna <aruna.evan15@gmail.com>
2021-10-14 14:39:50 -0700

    commit tree c593d689b9692ab76c913ce01c324f46a768e7f3

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // take out some of the elements if they are > the target
+  // no this doesn't change our big O
+  // a1 = [3, 4, 11, 12, 5, 7], target = 10
+  // sort and filter out ... going over the array 2x
+  // a2 = [3, 4, 5, 7] length = 2/3 length of a2
+  for (let beg=0; beg<array.length; beg++) {
+    for (let end=beg+1; end<array.length; end++){
+      if (array[beg] + array[end] === target) {
+          return true;
+        }
+    }
+  }
+  return false;
 }
+
+array = [3, 4, 11, 12, 5];
+target = 10;
+console.log(hasTargetSum(array,target));
+
+let n = array.length;
 
 /* 
   Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
+  - 
+
 */
 
 /*
   Add written explanation of your solution here
+  -  make a pointer to the first element
+  -  make a second pointer that will move over the other elements
+  -  add the two, and check if it is the target
+  -  if none for that round, then move first pointer to second element (repeat process)
+
+  -  sort the array (least to most)
+  -  ignore anything that is greater than our target
+  -  apply the above solution
+
 */
 
 // You can run `node index.js` to view these console logs
